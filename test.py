4973e7fe0d052f9fe4d23b4f84c6e3e2eb341323
yf <247820289@qq.com>
2023-02-02 18:29:04 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from generate import Single_Anchor, Anchors
+import numpy as np
+base_size = np.array(8)
+ratios = np.array([0.25, 1, 4])
+scales = np.array([4, 6, 8])
+
+anchors = Single_Anchor(base_size, scales, ratios)
+input_shape = np.array([2, 3])
+final_anchors = Anchors(input_shape, base_size, scales, ratios)
+print(final_anchors)
